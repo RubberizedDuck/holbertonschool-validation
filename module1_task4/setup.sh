9a20e9b9e34745391e0155498015195c725b59b1
RubberizedDuck <dyl_2536@hotmail.com>
2023-01-04 13:00:53 +1100

    Update setup to be run a sandbox env

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-apt-get update && apt-get install -y hugo make
+apt-get update && apt-get install -y hugo make curl build-essential
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" </dev/null
+echo '# Set PATH, MANPATH, etc., for Homebrew.' >> /root/.profile
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /root/.profile
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+brew install hugo
 make build
