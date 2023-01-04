4b8e90acb629549a8fc44607c03228df8419c584
RubberizedDuck <dyl_2536@hotmail.com>
2023-01-04 13:04:30 +1100

    Fix: change curl and installation of hugo

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-apt-get update && apt-get install -y hugo make curl build-essential
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" </dev/null
-echo '# Set PATH, MANPATH, etc., for Homebrew.' >> /root/.profile
-echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /root/.profile
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-brew install hugo
+apt-get update && apt-get install -y hugo make
+curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o hugo.deb
+apt install ./hugo.deb
+rm hugo.deb
 make build
